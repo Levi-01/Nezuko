cfae542a12b68ec7f87a670ce43e4727d967e119
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-02 03:19:42 +0300

    Create avatar.js

---
+++ komutlar/avatar.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+
+exports.run = function(client, message, args) {
+  
+const codework = message.mentions.users.first()
+let user;
+  
+if (message.mentions.users.first())  {user = message.mentions.users.first();}
+  
+else {user = message.author;}
+  
+return message.channel.send(new Discord.MessageEmbed()
+                            
+
+.setDescription(`[Avatar](${user.avatarURL()})`)                       
+.setFooter(user.tag).setTimestamp()                     
+.setImage(user.avatarURL({ format: 'png', dynamic: true, size: 1024})))
+
+};
+
+
+exports.conf = {
+    enabled: false,
+    guildOnly: false,
+    aliases: ['avatar','Avatar'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'avatar'
+    
+  };
