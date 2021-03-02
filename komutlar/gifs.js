d357c833f24c291422df259f7d3dcf5a423156d3
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-02 14:02:51 +0300

    Create gifs.js

---
+++ komutlar/gifs.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
+exports.run = function(client, message) {
+  
+  let prefix  = ayarlar.prefix
+
+const yardım = new Discord.MessageEmbed()
+.setColor('GREEN')
+.setAuthor(`GİF LANDS BOT - YARDIM MENÜSÜ`)
+.setDescription(`
+
+
+:white_small_square: **=**  \`?anime-gif\` : **Anime gif atar**
+:white_small_square: **=**  \`?man-gif\`:  **Adam gifi atar**
+:white_small_square: **=**  \`?baby-gif\` :  **Bebek gifi atar**
+:white_small_square: **=**  \`?woman-gif\` :  **Kadın gifi atar**
+:white_small_square: **=**  \`?animal-gif\`: **Hayvan gifi atar**
+:white_small_square: **=**  \`?atatürk\`: **Bir tane var sadece Atatürk gifi atar**
+:white_small_square: **=**  \`?porn-gif\`: **Porno Gifleri Atar :joy:**
+`)
+.setImage("https://api.creavite.co/out/84ba34f0-d8ba-49b7-9ecf-fc0849f2d671_standard.gif")
+.setThumbnail("https://cdn.discordapp.com/emojis/790170217900670986.gif?v=1")
+message.channel.send(yardım)
+
+  
+   
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: ['gifs'], 
+  permLevel: 0
+};
+
+exports.help = {
+  name: "gif",
+  description: 'Bizim yaptığımız bir gif kodu.',
+  usage: 'gif'
+};
