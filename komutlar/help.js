26f3d13569d1504b2b8da3fda04c83469990a793
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-04 22:10:06 +0300

    Update help.js

--- komutlar/help.js
+++ komutlar/help.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args, msg) => {
 
 const embed = new Discord.MessageEmbed()
     
-.setColor('#FFA500') 
+.setColor('#FFC0CB') 
 .setTitle(`My prefix is (${prefix})`)
 .setThumbnail(client.user.avatarURL())
 .setTimestamp()
