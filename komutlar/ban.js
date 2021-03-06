221aa38ebd4928b7091b2ccf4835d9f1e8300dd9
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 16:17:01 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,29 +1,32 @@
- const Discord = require('discord.js');
-//BLACK 
-exports.run = (client, message, args) => {
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have enough authority");
-    let kullanici = args[0];
-    if (!kullanici) return message.channel.send("You need to enter a user ID")
-    message.guild.fetchBans()
-        .then(bans => {
-            if (!bans.has(kullanici)) {
-                return message.channel.send(`This user is not banned.`)
-            }
-        })
-    message.guild.fetchBan(kullanici).then(({ user, reason }) => {
-        message.channel.send(`${user.tag} the reason for the ban of the user named: **${reason}**`)
+ const { MessageEmbed } = require('discord.js')
 
-    })
-}
+exports.run = async (client, message, args) => {
+  
+ if(!args[0])return message.channel.send(`Please specify the person you want to remove.`)
+        let User = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+        if(!User)return message.channel.send(`This is not a user on the server! Please specify a valid user.`)
+        let sebep = args.slice(1).join(" ")
+        if(!sebep) return message.channel.send(`Please give me a reason! You can t throw someone away for no reason, can you?`)
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`To use this command \`Ban Members\` you must have the authority.`);
+        message.guild.members.ban(User, { reason: sebep });
+        const Embed = new MessageEmbed()
+        .setTitle(`You Fired a User!`)
+        .setDescription(`${client.users.cache.get(User.id).username} Fired!`)
+        .setColor(`RANDOM`)
+        message.channel.send(Embed)
+        
+    }
+
+
 exports.conf = {
-    enabled: true,
+    enabled:false,
     guildOnly: false,
-    aliases: ['ban'],
-    permLevel: 0
-};
+    aliases: [],
+    permLevel: 3,
+}
 
 exports.help = {
-    name: 'ban-sorgu',
-    description: 'Ban sorgulama yaparsınız.',
-    usage: 'ban-sorgu'
-}; 
+    name: 'ban',
+    description: 'ban komutu',
+    usage: 'ban'
+} 
