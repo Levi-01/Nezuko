d355663e2763243cb86cc3701d5fd698a0025366
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 17:43:52 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,42 +1,42 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+    const Discord = require('discord.js');
 
-exports.run = async (client, message, args) => {
-  if(!message.member.roles.some(r => ["ROL ID" , "ROL ID"].includes(r.id)))
-  return message.reply("Bu komudu kulanmak için yeterli yetkiye sahip değilsin!") 
-
- 
-let unbanid = args[0]
-if(!unbanid) return message.reply("Banını kaldırmak istediğin kişinin id sini yazmalısın.")
-
-const unbanlog = message.guild.channels.find(unban => unban.id === "KANAL ID");
-
-  
-message.guild.unban(unbanid)
-message.channel.send(`<@${unbanid}>, Adlı Kullanıcının Banı Kaldırıldı.`).then(msg => msg.delete(5000))
- 
-
-  
-const unban = new Discord.RichEmbed()
-.setColor('BLACK')
-.setThumbnail(message.author.avatarURL)  
-.setDescription(`**• Asterion Moderation ! \n\n• Banı Kaldıran Yetkili :`+ message.member +`\n• Banı Kaldırılan Kullanıcı : <@${unbanid}>**`)
-.setTimestamp()
-.setImage('https://media2.giphy.com/media/P4bLhbzfxDaM0/giphy.gif?cid=790b7611927a1206a32a987fa98e88a78b2a26dcabe41fd6&rid=giphy.gif')
-.setFooter('Sadxmamy ❤️ Striga')
-unbanlog.send(unban)
-
-  
-
-};
-exports.conf = {
-enabled:true,
-guildOnly: true,
-aliases: ["unban"],
-permlevel: 2
-};
-exports.help = {
-name: "unban",
-description: "Herhangi bir kullanıcının banını açarsınız.",
-usage: "unban kullanıcı"
-};
+    module.exports = {
+        name: "unban",
+        description: "unbans a member from the server",
+        
+    
+        async run (client, message, args) {
+    
+            if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+            if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the permissions.')
+    
+            const member = message.mentions.members.first();
+    
+            if(!args[0]) return message.channel.send('Please specify a user');
+    
+    
+    
+            
+    
+            let reason = args.slice(1).join(" ");
+    
+            if(!reason) reason = 'Unspecified';
+    
+            message.guild.members.unban(`${member}`, `${reason}`)
+            .catch(err => {
+                if(err) return message.channel.send('Something went wrong')
+            })
+    
+            const banembed = new Discord.MessageEmbed()
+            .setTitle('Member Unbanned')
+            .addField('User Unbanned', member)
+            .addField('Unbanned by', message.author)
+            .addField('Reason', reason)
+            .setFooter('Time Unbanned', client.user.displayAvatarURL())
+            .setTimestamp()
+    
+            message.channel.send(banembed);
+    
+    
+        }
+    }
