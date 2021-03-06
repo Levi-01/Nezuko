a385f5fc8f64804f9433de617de0f5907e8d3748
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 17:23:39 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,37 +1,42 @@
-const { MessageEmbed } = require('discord.js')
-const db = require('quick.db')
-const moment = require('moment')
-const ayarlar = require("../ayarlar.json")
-exports.run = async (client, message, args) => {
-  
-const permError = new MessageEmbed()
-    .setColor('RED')
-    .setTitle('Failed')
-    .setAuthor(message.author.tag, message.author.avatarURL({ size:1024, dynamic:true, format: "png"}))
-    .setDescription(`To Use This Command <@&${ayarlar.banYetkiliRolID}> You Must Have Authority!`) 
-  
-if (!message.member.roles.cache.has(ayarlar.banYetkiliRolID)) return message.channel.send(permError); 
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const banlog = message.guild.channels.cache.find(c => c.id === ayarlar.unbanLogKanalID)
+module.exports.run = async (client, message, args) => {
+  if(!message.member.roles.some(r => ["ROL ID" , "ROL ID"].includes(r.id)))
+  return message.reply("Bu komudu kulanmak için yeterli yetkiye sahip değilsin!") 
+
+ 
+let unbanid = args[0]
+if(!unbanid) return message.reply("Banını kaldırmak istediğin kişinin id sini yazmalısın.")
+
+const unbanlog = message.guild.channels.find(unban => unban.id === "KANAL ID");
 
   
-let kisi = await client.users.fetch(args[0]);
-if(!kisi) return message.channel.send(new MessageEmbed().setDescription(`${message.author} you must specify an ID.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+message.guild.unban(unbanid)
+message.channel.send(`<@${unbanid}>, Adlı Kullanıcının Banı Kaldırıldı.`).then(msg => msg.delete(5000))
+ 
 
-message.guild.members.unban(kisi.id)
-message.channel.send(new MessageEmbed().setDescription(`${message.author} By ${kisi} user's server ban has been lifted.`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic:true }))).then(x => x.delete({ timeout: 5000}))
   
-message.react('✅')
-banlog.send(new MessageEmbed().setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp().setDescription(`**Sunucudan Yasağı Kaldırıldı !**\n**Kaldıran Yetkili:** ${message.author} (\`${message.author.id}\`) \n**Banı Kaldırılan Üye:** ${kisi} (\`${kisi.id}\`) \n**Ban Kaldırma Tarihi:** \`${moment(Date.now()).add(10,"hours").format("HH:mm:ss DD MMMM YYYY")}\` `));
+const unban = new Discord.RichEmbed()
+.setColor('BLACK')
+.setThumbnail(message.author.avatarURL)  
+.setDescription(`**• Asterion Moderation ! \n\n• Banı Kaldıran Yetkili :`+ message.member +`\n• Banı Kaldırılan Kullanıcı : <@${unbanid}>**`)
+.setTimestamp()
+.setImage('https://media2.giphy.com/media/P4bLhbzfxDaM0/giphy.gif?cid=790b7611927a1206a32a987fa98e88a78b2a26dcabe41fd6&rid=giphy.gif')
+.setFooter('Sadxmamy ❤️ Striga')
+unbanlog.send(unban)
 
-}
-  exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ["unban", "yasak-kaldır"],
-  permLvl: 0,
-}
+  
 
-  exports.help = {
-  name: 'unban'
-}
+};
+exports.conf = {
+enabled:true,
+guildOnly: true,
+aliases: ["unban"],
+permlevel: 2
+};
+exports.help = {
+name: "unban",
+description: "Herhangi bir kullanıcının banını açarsınız.",
+usage: "unban kullanıcı"
+};
