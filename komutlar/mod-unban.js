c5f658bfca4d5ddc222b4b4f0745de7accf04c16
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 18:04:06 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,45 +1,50 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-exports.run = (client, message, args) => {
+exports.run = async(client, message, args) => {
+  
 
-  if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`unban` adlı komutu özel mesajlarda kullanamazsın.')
-  return message.author.send(ozelmesajuyari); }
-  let guild = message.guild
-  let reason = args.slice(1).join(' ');
-  client.unbanReason = reason;
-  client.unbanAuth = message.author;
-  let user = args[0];
-  let modlog = guild.channels.find('name', 'ban-log');
-  if (!modlog) return message.reply('`ban-log` kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Ban kaldırma sebebini yazmalısın.');
-  if (!user) return message.reply('Banı kaldırılacak kişinin ID numarasını yazmalısın.').catch(console.error);
-  message.guild.unban(user);
+ var guild = message.guild;
+ var banlayan = message.author.tag;
+ var kisi = message.mentions.users.first() || client.users.resolve(args[0]) || client.users.cache.find(u => u.username === args[0]) || client.users.cache.get(args[0]);
+ if(!kisi) return message.reply("Banlayacağım Kişiyi Belirtmen Gerekiyor `ID / @kullanici / username`")
+ //var gun = args.slice(1).join(' ') ? `${args.slice(1).join(' ')}` :"";
+ var neden = args.slice(1).join(' ') 
+ let banxx = await message.guild.fetchBans();
 
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('Eylem:', 'Ban kaldırma')
-    .addField('Kullanıcı:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Yetkili:', `${message.author.username}#${message.author.discriminator}`)
-    .addField('Sebep', reason);
-  return guild.channels.get(modlog.id).send(embed);
+if (!banxx.get(kisi.id)) return message.reply(":x: Kişi Yasaklanmamış")
+
+if(neden) {
+  try {
+  await message.channel.send(`${kisi.tag} adlı kullanıcının banı açıldı. \nNedeni: **${neden}**`)
+  await guild.members.unban(kisi.id, neden);
+} catch (error) {
+  message.reply("Bir Sorun Oldu Lütfen Botun Geliştiricisi veya Yapımcısıyla İletişime Geçiniz!")
+  console.log(error)
+}
+} else {
+  try {
+    await message.channel.send(`${kisi.tag} adlı kullanıcının banı açıldı.`)
+    await guild.members.unban(kisi.id, neden);
+  } catch (error) {
+    message.reply("Bir Sorun Oldu Lütfen Botun Geliştiricisi veya Yapımcısıyla İletişime Geçiniz!")
+    console.log(error)
+  }
+
+}
+
+
+
+
 };
 
+
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['unban'],
+  aliases: [],
   permLevel: 2
 };
 
 exports.help = {
   name: 'unban',
-  description: 'İstediğiniz kişinin banını kaldırır.',
-  usage: 'unban [kullanıcı] [sebep]'
+  description: 'Botun Pingini Gösterir !',
+  usage: 'unban'
 };
