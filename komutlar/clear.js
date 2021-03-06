8ab1e0af128ea984b59875600963bbb175e08326
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 05:18:47 +0300

    Update clear.js

--- komutlar/clear.js
+++ komutlar/clear.js
@@ -1,37 +1,48 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
 
-exports.run = function(client, message, args) {
+exports.run = async (client, message, args) => {
+
   
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`Bu komutu kullanabilmek için **Mesajları Yönet** iznine sahip olmalısın!`);
-  
-if(isNaN(args[0])) {
-  var errembed = new Discord.MessageEmbed()
+  let embed = new Discord.MessageEmbed()
+    .setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true }))
     .setColor("RANDOM")
-    .addField(`Misuse!`, `You should write a number!`)
-    .addField(`Proper Use:`, `${ayarlar.prefix}clear < number of messages to clear >`)
-return message.channel.send(errembed);
-}
-  
-if (args[0] < 1) return message.reply("**1** I can't delete less than menstrual messages!")
-if (args[0] > 100) return message.reply("**100** I can't delete more than menstrual messages!")
-  
-message.channel.bulkDelete(args[0]).then(deletedMessages => {
-if (deletedMessages.size < 1) return message.reply("I couldn't delete any messages.! _(**14** I can't delete messages before the day!)_");
-})
-message.channel.send(`**${args[0]}** the custom message was successfully deleted! `)
+    .setTimestamp();
+
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send(embed
+      .setDescription("To use this command, you must have **Manage Messages** permission!"))
+      .then(x => x.delete({ timeout: 5000 }));
+
+
+    if(!args[0] ||
+    (args[0] && isNaN(args[0])) ||
+    Number(args[0]) < 1 ||
+    Number(args[0]) > 100 )
+
+    return message.channel.send(embed
+      .setDescription("1-100 must specify the amount of messages to delete between!"))
+      .then(x => x.delete({ timeout: 5000 }));
+
+
+  await message.delete()
+  .catch();
+
+  message.channel
+    .bulkDelete(Number(args[0]))
+    .then(mesajlar => message.channel.send(embed
+
+    .setDescription(`Başarıyla **${mesajlar.size}** adet mesaj silindi `))
+    .then(x => x.delete({ timeout: 5000 })))
+    .catch();
 };
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-  aliases: ["sil", "clear", "prune"],
-  permLevel: `Mesajları yönet yetkisine sahip olmak gerekir.`
+  aliases: ["sil", "clear"],
+  usage: "temizle 1-100",
+  description: "Belirtilen mesaj sayısı kadar mesaj temizler."
 };
 
 exports.help = {
-  name: 'sil',
-  category: 'moderasyon',
-  description: 0,
-  usage: '.sil <miktar>'
+  name: "temizle"
 };
+
