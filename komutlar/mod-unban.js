035e7e70f27de69cffb8e61151049b0b5359511a
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 18:31:30 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,44 +1,54 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-
-exports.run = async (client, message, args) => {
-
-  const db = require('quick.db');
-  
-  const fynx = require("../ayarlar/bot.json");
-let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix;
-    
-  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:sag2:786703055889760346>  **Bu komutu kullanabilmek için "\`Üyeleri Yasakla\`" yetkisine sahip olmalısın.**`);
-  
-
-  let user = args[0];
-  let reason = args.slice(1).join(' ');
-  if (db.has(`log_${message.guild.id}`) === false) return message.channel.send(`<a:sag2:786703055889760346> **Mod Log Kanalı Ayarlanmamış | ${prefix}modlog #kanal**`);
-  let modlog = message.guild.channels.cache.get(db.fetch(`log_${message.guild.id}`).replace("<#", "").replace(">", ""));
- if (isNaN(user)) return message.channel.send('<a:sag2:786703055889760346> **Lütfen Banını Açmak İstediğiniz Üyeninin ID sini Girin**');
-  if (reason.length < 1) return message.channel.send('<a:sag2:786703055889760346> **Lütfen Sebep Giriniz**');
- 
-  
-  const embed = new Discord.MessageEmbed()
-  .setColor("#ffd100")
-  .addField('<a:sag2:786703055889760346> İşlem', 'Ban Kaldırma')
-  .addField('<a:sag2:786703055889760346> Banı Açılan Üye', `(${user})`)
-  .addField('<a:sag2:786703055889760346> Banı Açan Yetkili', `${message.author.username}#${message.author.discriminator}`)
-  .addField('<a:sag2:786703055889760346> Banı Açma Sebebi', "```" + reason + "```")
-  modlog.send(embed);
-  message.guild.members.unban(user);
-  
-
-  
-  const embed2 = new Discord.MessageEmbed()
-  .setColor("#ffd100")
-  .setDescription(`<a:sag2:786703055889760346> Belirtiğiniz İD'nin Banı Açıldı`)
-  message.channel.send(embed2)
-
-  
+const Discord = require('discord.js')
+const db = require('quick.db')
+const ayarlar = require("../ayarlar.json")
+exports.run = async(client, message, args) => {
+  let prefix = ayarlar.prefix
+  if(!message.member.hasPermission("BAN_MEMBERS")) return;
+  if(!message.guild.members.cache.get(client.user.id).hasPermission("BAN_MEMBERS")) return;
+     let kişi = message.mentions.users.first()
+     let sebep = args.slice(1).join(" ")
+     if(!kişi) {
+       const ikrud = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`${prefix}ban User`)
+return message.channel.send(ikrud)
+     }
+     if(!sebep) sebep = `Sebep: Belirtilmemiş`
+     if(kişi.id === message.guild.ownerID) {
+       const pekabot = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You Can t Discard a Server Owner!**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(pekabot)
+     }
+     if(kişi.id === client.user.id) {
+       const pekabot = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You re Going to Dump Me?**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(pekabot)
+     }
+     if(kişi.id === message.author.id) {
+       const peka = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You Can t Throw Yourself Off the Server!**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(peka)
+     }
+     message.guild.member(kişi).ban({ reason: `Reason: ${sebep} | By: ${message.author.tag}` })
+ const ikrudka = new Discord.MessageEmbed()
+.setColor("#ffcb00")
+.setDescription(`**<@${kişi.id}> Named Member Banned From Server**`)
+return message.channel.send(ikrudka)
+   }
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['unban',"unbanned"],
+    permLevel: 0
 };
-
-exports.config = {
-  name: 'unban',
-  aliases: ['unban','yasak-kaldır','yasak-aç','ban-kaldır']
+  exports.help = {
+    name: 'unban',      
+    description: 'Belirtilen Kişiyi Sunucudan Kickler',
+    usage: 'unban <kullanıcı>'
 };
