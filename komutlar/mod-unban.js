b92e6032fa48d4552b36c93e93a6b590660b279b
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 17:47:49 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,42 +1,25 @@
-    const Discord = require('discord.js');
+const Discord = require(`discord.js`);
 
-    module.exports = {
-        name: "unban",
-        description: "unbans a member from the server",
-        
-    
-        async run (client, message, args) {
-    
-            if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
-            if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the permissions.')
-    
-            const member = message.mentions.members.first();
-    
-            if(!args[0]) return message.channel.send('Please specify a user');
-    
-    
-    
-            
-    
-            let reason = args.slice(1).join(" ");
-    
-            if(!reason) reason = 'Unspecified';
-    
-            message.guild.members.unban(`${member}`, `${reason}`)
-            .catch(err => {
-                if(err) return message.channel.send('Something went wrong')
-            })
-    
-            const banembed = new Discord.MessageEmbed()
-            .setTitle('Member Unbanned')
-            .addField('User Unbanned', member)
-            .addField('Unbanned by', message.author)
-            .addField('Reason', reason)
-            .setFooter('Time Unbanned', client.user.displayAvatarURL())
-            .setTimestamp()
-    
-            message.channel.send(banembed);
-    
-    
-        }
-    }
+exports.run = async(client, message, args) => {
+
+let User = args[0];
+let Reason = args.slice(1).join(` `);
+if (!User) return message.reply(`Who are we unbanning?`);
+if (!Reason) Reason = `Unbanned by ${message.author.tag}`
+
+message.guild.fetchBans()
+.then(bans => {
+if (bans.some(u => User.includes(u.username))) {
+let user = bans.find(user => user.username === User);
+
+message.guild.unban(user.id, Reason);
+}
+else if (bans.some(u => User.includes(u.id))) {
+
+message.guild.unban(User, Reason);
+}
+else {
+return message.reply(`This person is not banned`);
+}
+});
+}
