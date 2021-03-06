0f4949e4e7fd7773777e57cc9442ec588d5e6a6c
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-06 13:55:05 +0300

    Update nickname.js

--- komutlar/nickname.js
+++ komutlar/nickname.js
@@ -2,16 +2,16 @@ const discord = require('discord.js')
 
 exports.run = async(client, message, args) => {
 
-if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send(`Bu Komutu Kullanabilmek İçin **Kullanıcı Adlarını Yönet** Yetkisine Sahip Olmalısın.`);
+if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send(`To Use This Command, You Must Have **Manage UserNames** Authorization.`);
   
 let kinsta = message.mentions.members.first()
-if (!kinsta) return message.channel.send(`Bir kullanıcı etiketlemelisin.`)
+if (!kinsta) return message.channel.send(`You must tag a user.`)
 
 let isim = args.slice(1).join(' ')
-if (!kinsta) return message.channel.send(`Değiştirilicek ismi girin.`)
+if (!kinsta) return message.channel.send(`Enter the name to change.`)
 
 kinsta.setNickname(isim)
-message.channel.send(`${kinsta} Adlı Kullanıcının Yeni İsmi **${isim}\** Olarak Ayarlandı!`)
+message.channel.send(`${kinsta} User's New NickName Set to **${isim}\**!`)
 }
 
 exports.conf = {
