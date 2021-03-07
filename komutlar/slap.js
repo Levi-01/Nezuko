b4edd4bd7790201a2563120e6f978e9ae8884a5e
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-07 15:15:07 +0300

    Create slap.js

---
+++ komutlar/slap.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+const customisation = require('../customisation.json');
+
+exports.run = async (client, message, args, tools) => {
+    if (!message.mentions.users.first()) return message.reply("kime vurcam ");
+    if(message.mentions.users.first().id === "242263403001937920") return message.reply(':flushed:');
+    if (message.mentions.users.first().id == client.user.id && message.author.id === "526055697587240960"){
+      const { body } = await superagent
+      .get("https://nekos.life/api/v2/img/slap");
+      
+      const embed = new Discord.MessageEmbed()
+      .setColor("#ff9900")
+      .setTitle(`No u! *slaps*${message.mentions.users.first().username}`)
+      .setImage(body.url) 
+      .setTimestamp();
+      return message.channel.send({embed})
+    }else if (message.mentions.users.first().id == client.user.id && message.author.id !== "526055697587240960"){
+      return message.channel.send("NUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU ")
+    }
+    const { body } = await superagent
+    .get("https://nekos.life/api/v2/img/slap");
+    
+    const embed = new Discord.MessageEmbed()
+    .setColor("#ff9900")
+    .setTitle(`Nezuko, ${message.mentions.users.first().username} You got slapped by ${message.author.username}`)
+    .setImage(body.url) 
+    .setFooter(`© SFXBot by ${customisation.ownername}`);
+    message.channel.send({embed})
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['slap'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'slap',
+    description: 'Slaps someone OwO',
+    usage: 'slap'
+  };
