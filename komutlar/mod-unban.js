0a3df16fcec7685cbdd793250cb381bb563f55c2
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-08 21:24:17 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -1,35 +1,54 @@
 const Discord = require('discord.js')
-const datab = require('quick.db')
-const moment = require('moment')
-exports.run = async (client, message, args) => {
-
-  
-  
-if(![(config.banhammer)].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
-return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için <@&${config.banhammer}> rolune sahip olmalisin.`).setColor('RANDOM').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-
-
-
-
-
-  
-  
-let knaveqwe = await client.users.fetch(args[0]);
-if(!knaveqwe) return message.channel.send(new MessageEmbed().setDescription(`${message.author} bir @Mercy/İD belirtmelisin.`).setColor('RANDOM').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-
-message.guild.members.unban(knaveqwe.id)
-message.channel.send(new MessageEmbed().setDescription(`${message.author} tarafından ${knaveqwe} adlı kullanıcının sunucu yasağı kaldırıldı.`).setColor('RANDOM').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic:true }))).then(x => x.delete({ timeout: 5000}))
-  
-message.react((config.onayemoji))
-
-}
-  exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ["unban", "yasak-kaldır"],
-  permLvl: 0,
-}
-
+const db = require('quick.db')
+const ayarlar = require("../ayarlar.json")
+exports.run = async(client, message, args) => {
+  let prefix = ayarlar.prefix
+  if(!message.member.hasPermission("BAN_MEMBERS")) return;
+  if(!message.guild.members.cache.get(client.user.id).hasPermission("BAN_MEMBERS")) return;
+     let kişi = message.mentions.users.first()
+     let sebep = args.slice(1).join(" ")
+     if(!kişi) {
+       const ikrud = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`${prefix}unban User`)
+return message.channel.send(ikrud)
+     }
+     if(!sebep) sebep = `Sebep: Belirtilmemiş`
+     if(kişi.id === message.guild.ownerID) {
+       const pekabot = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You Can t Discard a Server Owner!**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(pekabot)
+     }
+     if(kişi.id === client.user.id) {
+       const pekabot = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You re Going to Dump Me?**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(pekabot)
+     }
+     if(kişi.id === message.author.id) {
+       const peka = new Discord.MessageEmbed()
+.setColor("#ff0000")
+.setDescription(`** You Can t Throw Yourself Off the Server!**`)
+.then(x => x.delete({ timeout: 5000 }));
+return message.channel.send(peka)
+     }
+     message.guild.member(kişi).unban({ reason: `Reason: ${sebep} | By: ${message.author.tag}` })
+ const ikrudka = new Discord.MessageEmbed()
+.setColor("#ffcb00")
+.setDescription(`**<@${kişi.id}> Named Member UnBanned From Server**`)
+return message.channel.send(ikrudka)
+   }
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['unban',"unbanned"],
+    permLevel: 0
+};
   exports.help = {
-  name: 'unban'
-}
+    name: 'unban',      
+    description: 'Belirtilen Kişiyi Sunucudan Kickler',
+    usage: 'unban <kullanıcı>'
+};
