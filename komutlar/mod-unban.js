65fae4fec1aad04313f2d60c4d3b42b555b65db7
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-08 21:42:34 +0300

    Update mod-unban.js

--- komutlar/mod-unban.js
+++ komutlar/mod-unban.js
@@ -3,52 +3,35 @@ const db = require('quick.db')
 const ayarlar = require("../ayarlar.json")
 exports.run = async(client, message, args) => {
   let prefix = ayarlar.prefix
-  if(!message.member.hasPermission("BAN_MEMBERS")) return;
-  if(!message.guild.members.cache.get(client.user.id).hasPermission("BAN_MEMBERS")) return;
-     let kişi = message.mentions.users.first()
-     let sebep = args.slice(1).join(" ")
-     if(!kişi) {
-       const ikrud = new Discord.MessageEmbed()
-.setColor("#ff0000")
-.setDescription(`${prefix}unban User`)
-return message.channel.send(ikrud)
-     }
-     if(!sebep) sebep = `Sebep: Belirtilmemiş`
-     if(kişi.id === message.guild.ownerID) {
-       const pekabot = new Discord.MessageEmbed()
-.setColor("#ff0000")
-.setDescription(`** You Can t Discard a Server Owner!**`)
-.then(x => x.delete({ timeout: 5000 }));
-return message.channel.send(pekabot)
-     }
-     if(kişi.id === client.user.id) {
-       const pekabot = new Discord.MessageEmbed()
-.setColor("#ff0000")
-.setDescription(`** You re Going to Dump Me?**`)
-.then(x => x.delete({ timeout: 5000 }));
-return message.channel.send(pekabot)
-     }
-     if(kişi.id === message.author.id) {
-       const peka = new Discord.MessageEmbed()
-.setColor("#ff0000")
-.setDescription(`** You Can t Throw Yourself Off the Server!**`)
-.then(x => x.delete({ timeout: 5000 }));
-return message.channel.send(peka)
-     }
-     message.guild.member(kişi).unban({ reason: `Reason: ${sebep} | By: ${message.author.tag}` })
- const ikrudka = new Discord.MessageEmbed()
-.setColor("#ffcb00")
-.setDescription(`**<@${kişi.id}> Named Member UnBanned From Server**`)
-return message.channel.send(ikrudka)
-   }
-exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ['unban',"unbanned"],
-    permLevel: 0
+  
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
+        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+        if (!User) return message.channel.send("Invalid User")
+        if (User.hasPermission("BAN_MEMBERS")) return message.reply("Can't ban that one, he also can ban")
+        let banReason = args.join(" ").slice(22);
+        if (!banReason) {
+            banReason = "None"
+        }
+        console.log(`USER = ${User}`)
+        User.unban({reason: banReason})
+        var UserID = User.id
+        console.log(`USER ID = ${UserID}`)
+    }
+
+  
+  
+  
+  
+  
+  exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['unban'],
+  permLevel: 0,
 };
+
   exports.help = {
-    name: 'unban',      
-    description: 'Belirtilen Kişiyi Sunucudan Kickler',
-    usage: 'unban <kullanıcı>'
+  name: 'unban',
+  description: 'partical',
+  usage: 'unban'
 };
