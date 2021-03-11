d62de373ecb0b3560fa3adfc363bf0e7111efce3
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-11 08:57:12 +0300

    Create fun-cat.js

---
+++ komutlar/fun-cat.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+exports.run = async (bot, message, args) => { 
+    const apiKey = message.client.apiKeys.catApi;
+    try {
+      const res = await fetch('https://api.thecatapi.com/v1/images/search', { headers: { 'x-api-key': apiKey }});
+      const img = (await res.json())[0].url;
+      const embed = new Discord.MessageEmbed()
+        .setTitle('🐱  Meow!  🐱')
+        .setImage(img)
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      message.channel.send(embed);
+    } catch (err) {
+      message.client.logger.error(err.stack);
+      this.sendErrorMessage(message, 1, 'Please try again in a few seconds', err.message);
+    }
+  }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["cat"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "cat",
+  description: "meow",
+  usage: "cat"
+};
