1c267b732c5a6c711befec2e1c2c834b8b5a537b
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-13 02:57:30 +0300

    Update mod-ban.js

--- komutlar/mod-ban.js
+++ komutlar/mod-ban.js
@@ -1,73 +1,52 @@
-const Discord = require ('discord.js');
-const db = require ('quick.db');
+exports.run = (client, message, args, Discord, sql) => {
+  var react = require("../eos.js")
+  var guild = message.guild
+  //The targeted channel (aka mod log channel)
+  //the user to be banned
+  var banneduser = message.mentions.users.first()
+  if(banneduser.id == client.user.id){
+    return message.channel.send("Try banning me again you fuckwit.")
+  }
+  //saves the ban perms in a compact variable
+  let banPerms = message.channel.guild.member(client.user.id).hasPermission("BAN_MEMBERS")
+  var reason = args.slice(1).join(" ");
+  if(!reason){
+    reason = "No Reason";
+  }
 
-exports.run = async (bot, message, args) => {
-        try {
-            if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**You Dont Have The Permissions To Ban Users! - [BAN_MEMBERS]**");
-            if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("**I Dont Have The Permissions To Ban Users! - [BAN_MEMBERS]**");
-            if (!args[0]) return message.channel.send("**Please Provide A User To Ban!**")
+  if(!guild.members.get(message.author.id).hasPermission("BAN_MEMBERS")){return react.noPermReact()};
 
-            let banMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
-            if (!banMember) return message.channel.send("**User Is Not In The Guild**");
-            if (banMember === message.member) return message.channel.send("**You Cannot Ban Yourself**")
+  //if the bot doesn't have the permissions
+  if(!banPerms){return react.noPermReact()
+  }else{
+      //if the user is bannable
+      if (message.mentions.users.first().bannable = true){
+        //send the confirmation message, add a react and ban the user
+        if (args.length >= 2){
+        message.guild.member(banneduser).ban()
+        .then(message.channel.send("Shade \`Success`\ - User banned successfully.")
+        .then(message=>message.react('✅')));
+        //builds the embed for the log channelOh
+        const embed = new Discord.RichEmbed()
+          .setColor(message.guild.member(client.user).highestRole.color)
+          .setTimestamp(message.createdAt)
+          .addField("User Banned: ", banneduser, true)
+          .addField("Banned By: ", message.author.username, true)
+          .addField("Reason: ", reason, true)
+          .setFooter("Automated Mod Logging");
+          //sends the embed
 
-            var reason = args.slice(1).join(" ");
+          const config = require ("../config.json")
+          const logchannel = message.guild.channels.get(config[guild.id].modlogchannelID)
+          if(!logchannel || logchannel === "null"){
+            logchannel = message.guild.channels.get(config[guild.id].logchannelID)
+          }
+          logchannel.send(`**Infraction for: **${message.mentions.users.first()}`, {embed}).catch(console.log)
 
-            if (!banMember.bannable) return message.channel.send("**Cant Kick That User**")
-            try {
-            banMember.send(`**Hello, You Have Been Banned From ${message.guild.name} for - ${reason || "No Reason"}**`).then(() =>
-                message.guild.members.ban(banMember, { days: 7, reason: reason })).catch(() => null)
-            } catch {
-                message.guild.members.ban(banMember, { days: 7, reason: reason })
-            }
-            if (reason) {
-            var sembed = new Discord.MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${banMember.user.username}** has been banned for ${reason}`)
-            message.channel.send(sembed)
-            } else {
-                var sembed2 = new Discord.MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${banMember.user.username}** has been banned`)
-            message.channel.send(sembed2)
-            }
-            let channel = db.fetch(`modlog_${message.guild.id}`)
-            if (channel == null) return;
-
-            if (!channel) return;
-
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
-                .setColor("#ff0000")
-                .setThumbnail(banMember.user.displayAvatarURL({ dynamic: true }))
-                .setFooter(message.guild.name, message.guild.iconURL())
-                .addField("**Moderation**", "ban")
-                .addField("**Banned**", banMember.user.username)
-                .addField("**ID**", `${banMember.id}`)
-                .addField("**Banned By**", message.author.username)
-                .addField("**Reason**", `${reason || "**No Reason**"}`)
-                .addField("**Date**", message.createdAt.toLocaleString())
-                .setTimestamp();
-
-            var sChannel = message.guild.channels.cache.get(channel)
-            if (!sChannel) return;
-            sChannel.send(embed)
-        } catch (e) {
-            return message.channel.send(`**${e.message}**`)
-        }
+        }else{
+          message.reply("Shade \`Error`\ - You must add a reason!")
+          .then(message=>message.react('❎'));
+      }
     }
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["ban"],
-  permLevel: 0
-};
-
-exports.help = {
-  name: "ban",
-  description: "banned",
-  usage: "[name | nickname | mention | ID] <reason> (optional)"
-};
+  }
+}
