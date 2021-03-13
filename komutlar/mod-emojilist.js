51f3d27aef5c18a66de629cb2fa7aaa530a028b1
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-14 02:06:13 +0300

    Update mod-emojilist.js

--- komutlar/mod-emojilist.js
+++ komutlar/mod-emojilist.js
@@ -9,21 +9,21 @@ exports.run = async(client, msg, args) => {
         guild.emojis.cache.forEach((e) => {
             e.animated ? animEmotes.push(`<a:${e.name}:${e.id}>`) : staticEmotes.push(`<:${e.name}:${e.id}>`);
         });
-        staticEmotes = staticEmotes.length !== 0 ? `__**[${staticEmotes.length}] Normal Emoji**__\n${staticEmotes.join('')}` : '\n**Normal Emoji Bulunmuyor**';
-        animEmotes = animEmotes.length !== 0 ? `\n\n__**[${animEmotes.length}] Hareketli Emoji**__\n${animEmotes.join('')}` : '\n**Hareketli Emoji Bulunmuyor**';
+        staticEmotes = staticEmotes.length !== 0 ? `__**[${staticEmotes.length}] Normal Emoji**__\n${staticEmotes.join('')}` : '\n**Normal Emojis**';
+        animEmotes = animEmotes.length !== 0 ? `\n\n__**[${animEmotes.length}] Hareketli Emoji**__\n${animEmotes.join('')}` : '\n**Moving Emojis**';
         try {     
   let botembed = new Discord.MessageEmbed()
             .setColor(`RANDOM`)
             .setDescription(staticEmotes + animEmotes)
-            .setAuthor(`${msg.guild.name} Sunucusu Emojileri`, msg.guild.iconURL())
-    .setFooter('Bu komutu kullanan kullanıcı ' + msg.author.tag, msg.author.avatarURL())
+            .setAuthor(`${msg.guild.name} Server Emojis `, msg.guild.iconURL())
+    .setFooter('User using this command ' + msg.author.tag, msg.author.avatarURL())
             .setTimestamp()
         return msg.channel.send(botembed)
       } catch (err) {
         const embed = new Discord.MessageEmbed()
-            .addField(`Sunucuda Bulunan Emojiler`, 'Üzgünüm ama sunucunuzda ya çok fazla emoji bulunuyor ya da hiç emoji bulunmuyor. Bunları gösteremiyorum. Discord buna izin vermiyor.')
+            .addField(`Emojis On The Server`, 'Im sorry but your server either has too many emojis or you don t have any emojis at all I can t show them Discord won t allow it..')
             .setColor('RED')
-          .setFooter('Bu komutu kullanan kullanıcı ' + msg.author.tag, msg.author.avatarURL())
+          .setFooter('User using this command ' + msg.author.tag, msg.author.avatarURL())
             .setTimestamp()
         msg.channel.send(embed)
                               
@@ -31,7 +31,7 @@ exports.run = async(client, msg, args) => {
 }
 //GNARGE BOT ALTYAPISIN'DAN BU DOSYAYA AKTARILMIŞTIR!
 exports.conf = {
- aliases: ['emoji-liste'],
+ aliases: ['emoji-liste','eomjilist'],
  permLevel: 0,
  kategori: 'Sunucu'
 };
