16575fa14879300268f9ea8a39e4c061eb5d9735
mhamad123github <69951026+mhamad123github@users.noreply.github.com>
2021-03-15 02:39:37 +0300

    Create gif-showemoji.js

---
+++ komutlar/gif-showemoji.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+       
+    try{
+
+        if (!args[0]) return message.channel.send("You have to specify an emoji.")//ustedes definen los args
+ 
+        var emoji = Discord.Util.parseEmoji(args[0]);
+
+        if(emoji.id == null) return message.channel.send("Please enter a valid emoji.");
+
+        const embedEmoji = new Discord.MessageEmbed()
+      
+        .setImage(`https://cdn.discordapp.com/emojis/${emoji.id}.${(emoji.animated ? 'gif': 'png')}`)
+        .setColor('RANDOM')
+        
+        message.channel.send(embedEmoji)
+
+        } catch(e) {
+        message.channel.send(`**${e.name}:** ${e.message}`);
+      };
+
+  }
+
+exports.conf = {
+
+  enabled: true,
+
+  guildOnly: false,
+
+  aliases: ['showemoji'],
+
+  permLevel: 0
+
+};
+
+exports.help = {
+
+  name: 'showemoji',
+
+  description: 'lrowsxrd',
+
+  usage: 'showemoji'
+
+};
