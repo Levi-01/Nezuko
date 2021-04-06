2a4580d5e3e14f08910bbabd995928a5d99b4eb5
IBOY <69951026+Levi-01@users.noreply.github.com>
2021-04-07 01:47:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -144,6 +144,7 @@ client.yetkiler = message => {
 
 client.on("guildCreate", guild => {
   client.channels.cache.get("828804920987090986")
+.react("✅")
 .send(`join server |🔺
 Server name: ${guild.name}
 Server owner: ${guild.owner}
