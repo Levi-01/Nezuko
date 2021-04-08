b2b6f3f28c251a42118524984cfabe37e7010889
IBOY <69951026+Levi-01@users.noreply.github.com>
2021-04-08 20:36:18 +0300

    Update bot-help.js

--- komutlar/bot-help.js
+++ komutlar/bot-help.js
@@ -15,6 +15,7 @@ const embed = new Discord.MessageEmbed()
 .setDescription(`[Add to your server](https://discord.com/api/oauth2/authorize?client_id=816089794002157598&permissions=8&scope=bot) **|** [Support](https://discord.gg/EKwREXxUdn)`)
 .addField("**🌙 Info Commands**", `invite • info • ping • info`, true)
 .addField("**🌙 Gifs Commands**", `anime • girl • boy • animal • couple • bff • movies • giphy • neon • smoke • streamer`, true)
+.addField("**🌙 Mod Commands**", `clear • lock • unlock • kick • ban • role • nickname • addemoji • emojilist`, true)
 
 
 .setFooter(`${message.author.tag} `, message.author.avatarURL)
