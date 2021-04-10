8637678c93138782c78cd2234636eb49ee2a83a4
IBOY <69951026+Levi-01@users.noreply.github.com>
2021-04-11 01:42:24 +0300

    Update photo-boy.js

--- komutlar/photo-boy.js
+++ komutlar/photo-boy.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let replies = ["https://i.pinimg.com/736x/35/2a/52/352a52e7443026e3aa4498c00fb709e8.jpg"]
+let replies = ["https://cdn.wallpapersafari.com/47/58/l0RTIB.jpg","https://i.pinimg.com/736x/35/2a/52/352a52e7443026e3aa4498c00fb709e8.jpg"]
 
 let result = Math.floor((Math.random() * replies.length));
 
